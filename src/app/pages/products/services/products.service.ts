31146e7091b571ef1d08f300c403e2b160e9dfc9
Haziel Revert <hazielrevert@gmail.com>
2023-01-24 09:29:00 -0300

    solucion de errores

--- src/app/pages/products/services/products.service.ts
+++ src/app/pages/products/services/products.service.ts
@@ -1,38 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, mergeMap, Observable, take, tap } from 'rxjs';
+import { mergeMap, take, tap } from 'rxjs';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Observable } from 'rxjs/internal/Observable';
 
 export interface Product {
-  createdAt: Date;
-  name: string; 
+  createdAt:   Date;
+  name:        string;
   description: string;
-  id: string;
+  id:          string;
 }
 
 export interface IProductsService {
-  products$: Observable<Product[]>;
-  loadProducts(): void;
   createProduct(data: Pick<Product, 'description' | 'name'>): void;
 }
 
-
-export class ProductsService implements IProductsService{
-  private readonly baseUrl = 'https://63c54104e1292e5bea1eae2c.mockapi.io';
-  private products =  new BehaviorSubject<Product[]>([])
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService implements IProductsService {
+  private readonly baseUrl = 'https://63b738094d97e82aa3b78a1a.mockapi.io';
+  private products = new BehaviorSubject<Product[]>([])
   public products$: Observable<Product[]>;
-  constructor(private httpClient: HttpClient) { 
+  constructor(private httpClient: HttpClient) {
     this.products$ = this.products.asObservable();
   }
-  createProducts(): void {
-    
-  }
-  
 
-  loadProducts(){
-    this.httpClient.get<Product[]>(`${this.baseUrl}/products`).subscribe((apiProducts) => {
-      this.products.next(apiProducts)
-    })
+  loadProducts() {
+    this.httpClient.get<Product[]>(`${this.baseUrl}/products`)
+      .subscribe((apiProducts) => {
+        this.products.next(apiProducts)
+      })
   }
+
   createProduct(data: Pick<Product, 'description' | 'name'>) {
     this.products$
       .pipe(
@@ -42,8 +42,7 @@ export class ProductsService implements IProductsService{
             this.httpClient.post<Product>(`${this.baseUrl}/products`, data)
               .pipe(
                 tap(
-                  (createdProduct => this.products.next([
-                    ...currentProducts, createdProduct]))
+                  (createdProduct => this.products.next([...currentProducts, createdProduct]))
                 )
               )
         ),
